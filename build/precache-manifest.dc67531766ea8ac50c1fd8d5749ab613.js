4171a2c46f5a0ea40812e8160ce4ef88485f2a51
7zhk <dev@sgis.site>
2020-08-23 13:34:38 +0800

    使用react重构GithubPage

---
+++ build/precache-manifest.dc67531766ea8ac50c1fd8d5749ab613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8949cdab32a557d8d3eba7e65315112a",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "a2010475575f7ee9f6c1",
+    "url": "/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "699bb86c99a80d90d35e",
+    "url": "/build/static/js/2.3ab16e9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/build/static/js/2.3ab16e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2010475575f7ee9f6c1",
+    "url": "/build/static/js/main.8de751d4.chunk.js"
+  },
+  {
+    "revision": "0ab9e0a8ba679e0f0f62",
+    "url": "/build/static/js/runtime-main.b3cdedbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
