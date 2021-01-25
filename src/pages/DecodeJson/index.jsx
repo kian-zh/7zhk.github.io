e0ceefc9fbc12b54d3b6751bba428be53def029c
Kian Zhang  - RKian.ZhangD <Kian.Zhang@aztech.com>
2021-01-25 17:33:47 +0800

    用React Hooks构建JSON页面

---
+++ src/pages/DecodeJson/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import style from './index.module.less'
+
+function DecodeJson () {
+    const [jsonData, setJsonData] = useState({});
+
+    return 
+    <div>
+        Hello
+    </div>
+}
+
+export default DecodeJson
