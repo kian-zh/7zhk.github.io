129b729ff19feb55f648c6d0dc5d23ad94d79ac3
Kian Zhang  - RKian.ZhangD <Kian.Zhang@aztech.com>
2021-02-04 16:33:58 +0800

    新增部分json解析

--- src/pages/DecodeJson/index.jsx
+++ src/pages/DecodeJson/index.jsx
@@ -1,38 +1,75 @@
 import React, { useState, useEffect } from 'react'
-//  暂不使用防抖
-//  import _ from "lodash";
 import style from './index.module.less'
 
 function DecodeJson () {
-    const [jsonData, setJsonData] = useState('');
+    const [jsonText, setJsonText] = useState('');
+    const [jsonObj, setJsonObj] = useState({});
     const [expandJson, setExpandJson] = useState({});
+    const [error, setError] = useState('');
+
     useEffect(() => {
-        console.log(jsonData)
-        //  _.debounce(()=>{console.log('1')},500,{ 'maxWait': 1000 })
+        console.log('realod')
     });
 
     const JsonDataUpdate = (e)=>{
-        setJsonData(e.currentTarget.value)
-    };
+        const localJsonText = e.currentTarget.value
+        const localJsonObj = JSON.parse(localJsonText)
+        try{
+            const localExpandNode = {}
+            addExpandNode(localJsonObj,localExpandNode)
+            setError('')
+            setExpandJson(localExpandNode)
+        }catch(error){
+            setError(error.toString())
+        }
+        setJsonObj(localJsonObj)
+        setJsonText(localJsonText)
+    }
+
+    const addExpandNode = (json,expandNode) => {
+        if(json instanceof Object){
+            const keys = Object.keys(json)
+            expandNode[0] = false
+            keys.forEach((key)=>{
+                expandNode['$_'+key]={}
+                addExpandNode(json[key],expandNode['$_'+key])
+            })
+        }
+    }
+
+    const changeExpandNode = (path) => {
+        const localExpandJSON = expandJson
+        const node = localExpandJSON
+        path.forEach((pa)=>{
+            node = node[pa]
+        })
+        node[0] = ! node[0]
+        setExpandJson(localExpandJSON)
+    }
+
+    const renderLines = () => {
+
+    }
 
     const renderJson = ()=>{
-        try{
-            const obj = JSON.parse(jsonData);
+        if(error){
+            return <h4>{error}</h4>
+        }else{
+            const lines = []
+            
+            
+            return <p>{JSON.stringify(expandJson)}</p>
         }
-        catch(error){
-            return (<h4>{error.toString()}</h4>)
-        }
-        return <h4>{jsonData}</h4>
     }
 
     return (
     <div>
         <section className={style.leftpart}>
-            <textarea className={style.inputBox} onKeyUp={(e)=>{JsonDataUpdate(e)}}>
+            <textarea className={style.inputBox} onChange={(e)=>{JsonDataUpdate(e)}}>
             </textarea>
         </section>
         <section className={style.rightpart}>
-            <div>
+            <div className={style.outputBox}>
                 {renderJson()}
             </div>
         </section>
